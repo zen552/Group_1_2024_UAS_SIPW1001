b1bd220332b54fe99dc35c13d33b23828714ac4b
zen552 <alvinbungut743@gmail.com>
2024-12-29 05:37:49 +0700

    Arrays

--- Arrays/floyd.py
+++ Arrays/floyd.py
@@ -1,9 +1,77 @@
-def floyd_triangle(n):
-    num = 1
-    for i in range(1, n + 1):
-        for j in range(i):
-            print(num, end=" ")
-            num += 1
-        print()
+class Node:
+    def __init__(self, val=None, nxt=None):
+        self.val = val
+        self.nxt = nxt
+    def __repr__(self):
+        return f"Node({self.val})"
 
-floyd_triangle(5)
+class LinkedList:
+    def __init__(self, head=None):
+        self.head = head
+
+def cycle(llist):
+    ptr = llist.head
+    visited = set()
+    while ptr is not None:
+        if ptr in visited:
+            return ptr
+        else:
+            visited.add(ptr)
+            ptr = ptr.nxt
+    return None
+
+def cycle(llist):
+    slow = llist.head
+    fast = llist.head
+    met = False
+    while fast is not None and fast.nxt is not None:
+        slow = slow.nxt
+        fast = fast.nxt.nxt
+        if slow == fast:
+            met = True
+            break
+    if not met:
+        return None
+    else:
+        slow = llist.head
+        while slow != fast:
+            slow = slow.nxt
+            fast = fast.nxt
+    return slow
+
+def find_duplicate(arr):
+    visited = set()
+    for elem in arr:
+        if elem in visited:
+            return elem
+        else:
+            visited.add(elem)
+
+def find_duplicate(arr):
+    slow = 0
+    fast = 0
+    while True:
+        slow = arr[slow]
+        fast = arr[arr[fast]]
+        if slow == fast:
+            break
+    slow = 0
+    while slow != fast:
+        slow = arr[slow]
+        fast = arr[fast]
+    return slow
+
+#Contoh Penggunaan Floyd's Algorithm
+# Membuat Linked List dengan siklus
+node1 = Node(1)
+node2 = Node(2)
+node3 = Node(3)
+node4 = Node(4)
+
+node1.nxt = node2
+node2.nxt = node3
+node3.nxt = node4
+node4.nxt = node2
+
+ll = LinkedList(node1)
+print(cycle(ll))
--- Arrays/kadane.py
+++ Arrays/kadane.py
@@ -6,5 +6,6 @@ def kadane(arr):
         max_sum = max(max_sum, current_sum)
     return max_sum
 
-arr = [-2, 1, -3, 4, -1, 2, 1, -5, 4]
+#Contoh penggunaan Kadane's Algorithm
+arr = [-2, 1, -3, 4, -1, 2, 1, 5, -4]
 print(kadane(arr)) 
